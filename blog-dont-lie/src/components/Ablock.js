cd3905fbaac19963aaec9d089e1f68bf69cab6e3
shehanpeiris <shehanpeiris@gmail.com>
2019-10-13 14:09:56 -0500

    added Ubuntu font

--- blog-dont-lie/src/components/Ablock.js
+++ blog-dont-lie/src/components/Ablock.js
@@ -7,8 +7,8 @@ class Ablock extends React.Component {
       <div className="a-block">
         <img className="a-block-image" src={this.props.image} alt="Virgil"></img>
         <section className="a-block-text">
-          <h1>{this.props.headline}</h1>
-          <p>Calm as you like, he's Virgil van Dijk. The centre-back has emerged as one of the world's best with all-around brilliance that anchors the Liverpool defense.</p>
+          <h1 className="a-block-hed"><strong>{this.props.headline}</strong></h1>
+          <p className="a-block-blurb">Calm as you like, he's Virgil van Dijk. The centre-back has emerged as one of the world's best with all-around brilliance that anchors the Liverpool defense.</p>
         </section>
       </div>
     )
