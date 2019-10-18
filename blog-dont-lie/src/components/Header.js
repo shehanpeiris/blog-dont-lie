5e3579c217fcd5389e683d03b3faacd71e3d2a0b
shehanpeiris <shehanpeiris@gmail.com>
2019-10-18 18:26:51 -0500

    installed classnames package, added header animation on scroll

--- blog-dont-lie/src/components/Header.js
+++ blog-dont-lie/src/components/Header.js
@@ -1,12 +1,35 @@
 import React from "react";
 import banner from "./images/headerv3.png";
 import Navbar from "./Navbar";
+import classnames from "classnames";
 
 class Header extends React.Component { 
+  state = {
+    visible: true,
+    prevScrollPos: window.pageYOffset
+  };
+
+   // Adds an event listener when the component is mount.
+   componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  
+  handleScroll = () => {
+    const {prevScrollPos} = this.state;
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollPos > currentScrollPos;
+    this.setState({
+      prevScrollPos: currentScrollPos,
+      visible
+    });
+  };
+
   render() {
     return (
       <div className="header">
-        <img className="banner-image" src={banner} />
+        <img
+          className={classnames("banner-image", {"banner-hidden": !this.state.visible})}
+          src={banner} />
         <Navbar />
       </div>
     )
