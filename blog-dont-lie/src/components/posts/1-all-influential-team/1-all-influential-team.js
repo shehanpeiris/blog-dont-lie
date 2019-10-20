98db878cb082797fa22672ac5376f658761919ba
shehanpeiris <shehanpeiris@gmail.com>
2019-10-19 19:16:29 -0500

    added images and specific css for all-influential team post

--- blog-dont-lie/src/components/posts/1-all-influential-team/1-all-influential-team.js
+++ blog-dont-lie/src/components/posts/1-all-influential-team/1-all-influential-team.js
@@ -1,5 +1,17 @@
 import React from "react";
 import {TwitterTweetEmbed} from "react-twitter-embed";
+import Draymond from "./images/draymond.jpg";
+import LeBron from "./images/lebron.jpg";
+import Pistons from "./images/pistons.jpg";
+import Steph from "./images/steph.jpg";
+import Jokic from "./images/jokic.jpg";
+import Duncan from "./images/duncan.jpg";
+import Manu from "./images/manu.jpg";
+import Lowry from "./images/lowry.jpg";
+import Dirk from "./images/dirk.jpg";
+import "./all-influential-team.css";
+
+
 
 
 const All_influential_team = () => (
@@ -15,35 +27,45 @@ const All_influential_team = () => (
     <p>After much deliberation, I came up with a 9-man rotation. I'm focusing solely on the "influential" part of Justin's question—these are the players that have molded the way I think about basketball.</p>
     <br/>
     <h3>Draymond Green</h3>
+    <img className="influential-image" src={Draymond} alt="draymond"/>
     <p>Nobody has shaped the way I think about defense more than Draymond Green. It's easy to spot all the events that take place on defense—a steal, a deflection, an emasculating rejection. Draymond does all these things fantastically well, but that's not what makes him special.</p>
     <p>The reason defense is so hard to quantify is that defense is more about the absence of events—the shot that is dissuaded, the drive that is stymied before it begins, the mismatch that evaporates before the offense can capitalize. Draymond's defensive precognition allows him to proactively shut down offensive plays before they even start. He has changed the way I look at defense, helping me focus on the events that never occur.</p>
     <br/>
     <h3>LeBron James</h3>
+    <img className="influential-image" src={LeBron} alt="lebron"/>
     <p>I grew up idolizing Michael Jordan. As a result, I was narrow-minded in my idea of what the best basketball player should look like and the role he/she should fill on their team. LeBron didn't do the same things that Jordan did, so how could Bron possibly be on the same level?</p>
     <p>Maybe the next GOAT candidate will remind us of Jordan or LeBron. But LeBron has taught me not to restrict my definition of basketball greatness to what I've already seen.</p>
     <br/>
     <h3>2004 Pistons</h3>
+    <img className="influential-image" src={Pistons} alt="pistons"/>
     <p>This team is the reason I obsess over fit, roles, and the synergies between players. These Pistons fit together so unbelievably well. Everyone knew their roles. They had JUST enough of everything you need to win big, and that was solely because of how well they played together and distinctly NOT because of the individual talent on their roster.</p>
     <p>The lessons I learned from the mid-2000s Pistons shape how I think about great teams today. Most contenders have more star power than those Pistons, but every great team needs role players, a hive mind and an identity.</p>
     <br/>
     <h3>Stephen Curry</h3>
+    <img className="influential-image" src={Steph} alt="steph"/>
     <p>I didn't think it was physically possible to shoot this well. Steph has made "gravity" common parlance on #NBATwitter, and it's because no player in the history of the league has more frequently made well-oiled defenses look like youth soccer teams. He has reframed my entire perspective about how to build around your best player and the limits (or lack thereof) of incendiary shooting.</p>
     <br/>
     <h3>Nikola Jokic</h3>
+    <img className="influential-image" src={Jokic} alt="jokic"/>
     <p>Jokic has made me question long-held beliefs about the need for "traditional" perimeter creation for high-level offensive success. I still think the Nuggets (or any team Jokic plays for) will ultimately need someone who can initiate offense as a primary ballhandler from the perimeter, if only to close playoff games. But the more I watch Big Honey, the more I start to wonder if he can actually do this thing himself.</p>
     <br/>
     <h3>Tim Duncan</h3>
+    <img className="influential-image" src={Duncan} alt="duncan"/>
     <p>Tim Duncan made me rethink how to evaluate true impact on winning with the endless positive contributions he made to the San Antonio Spurs dynasty. Whether it was rock-solid screens, consistent defensive rotations or being (from afar at least) the perfect teammate, Duncan did so many little things that compounded to make him the bedrock for a culture that resulted in two decades' worth of sustained brilliance, professionalism and adaptability.</p>
     <p>Culture matters, and Duncan is my constant reminder of that inescapable truth.</p>
     <br/>
     <h3>Manu Ginobili</h3>
+    <img className="influential-image" src={Manu} alt="manu"/>
     <p>I love Manu for a multitude of reasons, but he earned his place on this list because he gave me a completely new perspective on lineups and the patterns of a 48-minute NBA game. By embracing his 6th-man role, Ginobili optimized San Antonio's 48 minutes. Spurs benches ran roughshod over the league for most of Ginobili's career, largely because he was willing to accept a sixth-man role that more optimally distributed the Spurs' sources of shot creation.</p>
     <br/>
     <h3>Kyle Lowry</h3>
+    <img className="influential-image" src={Lowry} alt="lowry"/>
     <p>For a non-Raptors fan, I've thought about Kyle Lowry a lot over the last few years. He fascinates me because he symbolizes the importance of hierarchy on a team. If Kyle Lowry is your best player (as he was for so long in Toronto), your team has a hard ceiling. But slot Lowry to the #2 spot and magical things can happen. He has shaped the way I think about non-shot-creating stars and the tremendous (and necessary) value they add to championship-level teams.</p>
     <br/>
     <h3>Dirk Nowitzki</h3>
+    <img className="influential-image" src={Dirk} alt="dirk"/>
     <p>Watching Dirk Nowizki uncovered a new realm of possibilities regarding shot versatility. I had seen the Reggie Miller shooter archetype and its offspring (the souped-up Ray Allen version and the shorter-range Rip Hamilton model). Nowitzki's unparalleled combination of size, shooting stroke and shot diversity was a whole other animal.</p>
+    <p>Dirk taught me that knockdown shot-makers can make the best screeners and highlighted the value of non-traditional skillsets.</p>
   </div>
 );
 
