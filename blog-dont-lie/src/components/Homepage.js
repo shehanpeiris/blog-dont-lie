f350c00656627c3576f721057218300aae1bcb82
shehanpeiris <shehanpeiris@gmail.com>
2019-10-21 16:39:24 -0500

    added props material to a-block

--- blog-dont-lie/src/components/Homepage.js
+++ blog-dont-lie/src/components/Homepage.js
@@ -22,8 +22,9 @@ class Homepage extends React.Component {
       const postArr = data.reverse();
       console.log(postArr);
       this.setState({ablock: postArr[0]})
-      this.setState({cards: postArr.slice(1)});
-      
+      if (postArr.length > 1) {
+        this.setState({cards: postArr.slice(1)});
+      }  
       // this.setState({posts: data.reverse()})
     } // Used arrow function to access state
   )};
@@ -33,15 +34,20 @@ class Homepage extends React.Component {
       <div className="App">
         <Ablock 
           image={this.state.ablock.image}
-          headline={this.state.ablock.headline}
+          alt={this.state.ablock.alt}
           link={this.state.ablock.link}
+          headline={this.state.ablock.headline}
+          blurb={this.state.ablock.blurb}
         />
         <div className="card-wrapper">
           {Object.keys(this.state.cards).map(key => (
             <Card
-              key={key}
+              key={key}  
+              link={this.state.cards[key].link}
               image={this.state.cards[key].image}
+              alt={this.state.cards[key].alt}
               headline={this.state.cards[key].headline}
+              blurb={this.state.cards[key].blurb}
             />
           ))}
         </div>
