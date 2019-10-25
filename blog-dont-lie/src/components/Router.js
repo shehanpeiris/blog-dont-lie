53a9eaa56381b1ab23ad5587f17a9f892ff89730
shehanpeiris <shehanpeiris@gmail.com>
2019-10-24 19:26:20 -0500

    initial build of Lowry-Iguodala post, not rendering to local deploy

--- blog-dont-lie/src/components/Router.js
+++ blog-dont-lie/src/components/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Homepage from "./Homepage";
 import All_influential_team from "./posts/1-all-influential-team/1-all-influential-team";
+import Lowry_iguodala from "./posts/2-lowry-iguodala/2-lowry-iguodala";
 
 const Router = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const Router = () => (
       <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/1-all-influential-team" component={All_influential_team} />
+          <Route exact path="/2-lowry-iguodala-bliss" component={Lowry_iguodala} />
       </Switch>
   </BrowserRouter>
 );
---
+++ blog-dont-lie/src/components/posts/2-lowry-iguodala/2-lowry-iguodala.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./all-influential-team.css";
+
+const Lowry_iguodala = () => (
+  <div className="blog-post">
+    <h1>On Kyle Lowry, Andre Iguodala and Finding Your Bliss</h1>
+    <p>This offseason was a transition period for the NBA. The Golden State Warriors' reign of terror has (seemingly) ended, and the new season feels like the dawn of a new phase for the league. Much of my basketball reflection over the summer was spent pondering the last five years and what I've learned from them. The more I thought about it, the more my mind kept returning to two players that defined the period (for me) by finding the perfect environment for their unique talents: Andre Iguodala and Kyle Lowry.</p>
+    <p>The art of team-building in basketball fascinates me. The dynamic nature of the sport means interactions between teammates—whether they add to or detract from each other—are a chief consideration when GMs construct their rosters and coaches design their rotations. The best teams optimize their players by putting them in their ideal roles and building the right hierarchies.</p>
+    <p>For most superstars and role players, this tends to happen almost by default. Superstars define their environment; they are the sun and everything else orbits their immense talent. With role players, their talents are so specific that their roles are predetermined (e.g. Kyle Korver will run off screens and hit shots; Andre Roberson will defend the most dangerous perimeter threat).</p>
+    <p>It's the players in the middle that can sometimes fall by the wayside and never find their basketball nirvana. When the perfect harmony is struck and their role is perfectly optimized, these players can have an outsized impact on winning. That's what happened with Iguodala and Lowry, and the results were beautiful to watch.</p>
+    <br/>
+    <h3>Andre Iguodala</h3>
+  </div>
+);
+
+export default Lowry_iguodala;
