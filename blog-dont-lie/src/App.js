9161f9387bb300146c970db2179c84e51b440b5c
shehanpeiris <shehanpeiris@gmail.com>
2019-11-13 13:04:23 -0600

    finished phoenix suns post

--- blog-dont-lie/src/App.js
+++ blog-dont-lie/src/App.js
@@ -8,6 +8,7 @@ import All_influential_team from "./components/posts/1-all-influential-team/1-al
 import Lowry_iguodala from "./components/posts/2-lowry-iguodala/2-lowry-iguodala";
 import Luka_trae from "./components/posts/3-luka-trae/3-luka-trae";
 import Lpdl_nov7 from "./components/posts/4-lpdl-nov-7/4-lpdl-nov-7";
+import Phoenix_suns from "./components/posts/5-phoenix-suns/5-phoenix-suns";
 
 
 class App extends React.Component {
@@ -24,6 +25,7 @@ class App extends React.Component {
               <Route exact path="/2-lowry-iguodala-bliss" component={Lowry_iguodala} />
               <Route exact path="/3-luka-trae-one-pass-away" component={Luka_trae} />
               <Route exact path="/4-lpdl-danny-green-league-pass-kyle-oquinn" component={Lpdl_nov7} />
+              <Route exact path="/5-phoenix-suns-hot-start" component={Phoenix_suns} />
           </Switch>
         </BrowserRouter>
       </div>
