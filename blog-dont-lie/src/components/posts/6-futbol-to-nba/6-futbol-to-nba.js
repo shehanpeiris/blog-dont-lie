1c6206ba720fb42b1121e63617cadfddf8a9d2bb
shehanpeiris <shehanpeiris@gmail.com>
2020-01-27 20:42:53 -0600

    wrote futbol post

---
+++ blog-dont-lie/src/components/posts/6-futbol-to-nba/6-futbol-to-nba.js
@@ -0,0 +1,96 @@
+import React from "react";
+import FeaturedImage from "./images/nba-soccerball.jpg";
+import ShotClock from ".images/shot-clock.jpg";
+import VAR from "./images/var.jpg";
+import FutbolLoan from "./images/futbol-loan.jpg";
+import Advantage from ".images/advantage.jpg";
+import FanSong from "./images/fan-song.jpg";
+
+const Futbol_to_NBA = () => ( 
+  <div className="blog-post">
+    <h1>5 Things the NBA Should Steal from Futbol</h1>
+    <img className="post-full-width-image" src={FeaturedImage} alt="nba-futbol-crossover"/>
+    <p><i>Disclaimer: I have a medical condition whereby saying/writing the word "soccer" make me projectile vomit everywhere. I was born in England, so it's "football". I live in America where there's a different "football". To avoid confusion and keep my keyboard vomit-free, soccer will hereafter be called "futbol".</i></p>
+    <p>Over the past year, I've rediscovered my love for futbol. After Michael Jordan retired in 1998, my basketball fandom waned and I dove headfirst (and then rolled 17 times like Neymar Jr.) into the Premier League. Eventually, my futbol interest dipped as I resumed my unhealthy obsession with the NBA. But now, like MJ in 1995, "I'M BACK" to loving futbol again.</p>
+    <p>In the process of watching a good bit of futbol over the past year, the contrast to basketball has been enjoyable—and in some cases, cause for envy. I present to you the top 5 things I want the NBA to steal from the world of futbol.</p>
+    <br/>
+    <h3>5. Games Starting On Time</h3>
+    <img className="post-full-width-image" src={ShotClock} alt="nba-shot-clock"/>
+    <p>It's hard to believe, I know, but in the Premier League, if a game is scheduled to start at 2pm, kick-off happens at 2:00 pm. What a bizarre, foreign concept for an NBA fan, where an 8pm game doesn't actually start until 8:10 or 8:15.</p>
+    <p>In the world of futbol, all the pre-game stuff is completed...pre-game and the game itself begins as listed on the schedule. Consider me jealous.</p>
+    <br/>
+    <h3>4. Video Assistant Referee (VAR)</h3>
+    <img className="post-full-width-image" src={VAR} alt="futbol-VAR"/>
+    <p>This would probably be a shock to most futbol fans since my read on the situation is that they mostly hate VAR (their replay review system) and its implementation. And there are problems with some of the futbol rule interpretations to be sure.</p>
+    <p>But the system feels incredibly efficient compared to the NBA's review system. Why on Earth do all 3 NBA officials need to congregate around a 10-inch monitor to judge an out-of-bounds call? It makes no sense and it takes far too long. We already have the replay center in Secaucus. Let Secaucus make the calls and only bring the on-court refs into the decision process if it's TRULY a close, judgment call.</p>
+    <p>Replay review makes a ton of sense and I believe it's a net-positive, but the NBA can speed up the process. Reviews are interminably long and pull the game further away from the beautiful "flow" that makes basketball so entertaining.</p>
+    <br/>
+    <h3>3. Loans</h3>
+    <img className="post-full-width-image" src={FutbolLoan} alt="futbol-loan"/>
+    <p>I know, this one ain't happening. But it would be so much damn fun so I'm going to make my ill-conceived case anyway.</p>
+    <p>In futbol, there are two major types of player movement: transfers and loans. Transfers are functionally similar to NBA trades, but instead of players going both ways, it's normally an exchange of cash (a transfer fee) for player, and the player makes a permanent move from Team A to Team B.</p>
+    <p>Futbol also provides the option of loaning a player to another team. This typically happens because a major team wants to find playing time for a younger prospect who isn't good enough to earn it in his current situation.</p>
+    <p>For example, Liverpool have a 22-year-old right winger named Harry Wilson. Wilson is young, talented, and possesses a dangerous left boot, but he would be hard-pressed to find much playing time at Liverpool right now given the talent on the squad and their lofty aspirations. So Liverpool loaned Wilson to another Premier League club (AFC Bournemouth).</p>
+    <p>Liverpool benefits by promoting the development of a young player while retaining control of his future. Wilson benefits by getting consistent playing time but still having a discernible path to playing at his preferred club (Liverpool). And Bournemouth benefit by adding someone to their team at a low financial cost who can do things [like this](https://youtu.be/2XoxA2CRptU?t=39).</p>
+    <p>How would loans work in an NBA context? Maybe they would involve young prospects like Harry Wilson, but the juiciest examples would occur at the other end of the talent spectrum with someone like Bradley Beal, for example.</p>
+    <p>The Wizards have been fun, but they're treading water until John Wall gets back next season. Beal trade rumors were swirling (before a pre-season extension made Beal untradeable this season), but did it really make sense for the Wizards to lose him permanently? Beal is REALLY good and is still only 26 years old. If Washington traded him,they would be lucky to get a player (present or future) as good as Beal. What if there were a middle ground where the Wizards could get some assets from a team like the Milwaukee Bucks to rent Beal for just this season?</p>
+    <p>Developing a real loan structure that works for all parties is very complicated and will never happen. But it's a fun hypothetical to daydream about.</p>
+    <br/>
+    <h3>2. Referees Playing an "Advantage"</h3>
+    <img className="post-full-width-image" src={Advantage} alt="futbol-advantage"/>
+    <p>In futbol, if a foul is committed but the wronged team still has the ball in a potential scoring situation, the referee can signal an advantage and allow things to play out for a short time (~5 seconds). If the team in possession quickly loses the ball, the ref retroactively calls the foul and awards the free kick.</p>
+    <p>This enforces the spirit of the foul rules: fouls should never help the defense. The advantage rule prevents defenses from committing fouls in disadvantageous situations to get out of jail.</p>
+    <p>It would be a major philosophical change for the NBA to adopt an "Advantage"—one that would quickly become nightmarish given the sheer number of events and potential advantage situations crammed into 48 minutes of NBA basketball.</p>
+    <p>What I'm proposing is the adoption of the "advantage" rule strictly as it pertains to "take" fouls to prevent fast breaks. Clear-path fouls (and the unending reviews that accompany them) are stupid. It doesn't matter whether the defender is ahead of or behind the offensive player.</p>
+    <p>What matters is that the current system allows for a legal play where a defender can commit a foul to drastically swing the pendulum from conceding "the most efficient play type in basketball" (transition) to "conceding a regular half-court possession", whilst simultaneously removing one of the most exciting plays in basketball (the fast break).</p>
+    <p>Not on my watch. If there's a fast-break and the defender commits a ticky-tac reach-in foul where Giannis Antetokounmpo is still barreling towards the rim, the refs should have the discretion to play an advantage, let the offense team carry on with their possession, and let the fans enjoy a thunderous Giannis jam.</p>
+    <br/>
+    <h3>1. Fan Songs</h3>
+    <img className="post-full-width-image" src={FanSong} alt="futbol-crowd"/>
+    <p>Maybe I've just watched too much basketball, but crowd chants in an NBA game are a snooze-fest. I get it, you love your best player, but an "MVP! MVP!" chant is tired and usually ludicrous (I love Devin Booker, but he's no MVP).</p>
+    <p>Oh, it's late in a close game and the opponent has the ball? Good thing the crowd is doing that painfully monotonous "De-fense! De-fense!" chant that is heard in every arena. Without it, the players would never have realized their objective was to prevent a basket.</p>
+    <p>NBA fans could learn a thing or three from the creativity of futbol fans. Teams have their own specific songs that echo around their home stadiums; fans come up with funny, revamped lyrics to popular songs to celebrate their players; some fans come up with biting chants to mock their opponents.</p>
+    <p>It makes for a fun, unique environment that heightens the bond between team and home fans while reinforcing the team's identity. Check out this emotional rendition of 'You'll Never Walk Alone" after Liverpool's miraculous win vs. Barcelona in last year's Champions League semi-final (one of the top-3 most incredible sporting events I have ever watched, up there with G6 of the 2013 Finals and G7 of the 2016 Finals).</p>
+    <div
+      className="video"
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+        height: 0
+      }}
+    >
+      <iframe style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", border: "5px solid #293462", borderRadius: "40px"}} src="https://www.youtube.com/embed/uOFMov4RR_Q" frameborder="0"  allowfullscreen></iframe>
+    </div>
+    <p>The closest examples I can think of in the NBA are:</p>
+    <p>"DEEEE-TROIT BASKETBAAAALLLLL" for the mid-2000s Pistons;</p>
+    <div
+      className="video"
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+        height: 0
+      }}
+    >
+      <iframe style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", border: "5px solid #293462", borderRadius: "40px"}} src="https://www.youtube.com/embed/CaIvvpjjzFQ" frameborder="0"  allowfullscreen></iframe>
+    </div>
+    <p>and the Miami Heat crowd singing "Seven Nation Army".</p>
+    <div
+      className="video"
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+        height: 0
+      }}
+    >
+      <iframe style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", border: "5px solid #293462", borderRadius: "40px"}} src="https://www.youtube.com/embed/bsWdGiofN_M" frameborder="0"  allowfullscreen></iframe>
+    </div>
+    <p>There is an obvious difference between the slower pace of futbol and the end-to-end nature of basketball. But can you just imagine a crowd singing/chanting in unison as the Milwaukee Bucks or Los Angeles Lakers are uncorking one of their dominant in-game runs? The crowd atmosphere in futbol can send tingles down your spine. Hopefully we can get there in the NBA one day.</p>
+    <br/>
+    <br/>
+  </div>
+);
+
+export default Futbol_to_NBA;
